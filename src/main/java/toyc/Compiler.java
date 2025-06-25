3016f28e9e1171e7f9d7c816068d555b6c0c9e05
Skye-ye <Sky_Zheng_@outlook.com>
2025-06-25 14:12:07 +0800

    feature: syntax highlighter and formatter

--- src/main/java/toyc/Compiler.java
+++ src/main/java/toyc/Compiler.java
@@ -4,8 +4,8 @@ import java.io.IOException;
 
 import toyc.semantic.SemanticChecker;
 import toyc.util.LexerErrorListener;
-import toyc.util.ParseTreePrinter;
 import toyc.util.ParserErrorListener;
+import toyc.util.formatter.ToyCFormatter;
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.ParseTree;
 
@@ -38,6 +38,10 @@ public class Compiler {
                 checker.visit(tree);
                 if (!checker.hasError()) {
                     System.out.println("No semantic errors found.");
+                    ToyCFormatter formatter = new ToyCFormatter();
+                    formatter.visit(tree);
+                    String formattedCode = formatter.getFormattedCode();
+                    System.out.println(formattedCode);
                 }
             }
         }
